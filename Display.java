85c0074bd08528f52eb890f1d87251a19c9528e0
Aysha-shamli <ayshashamli1999@gmail.com>
2023-11-16 13:05:14 +0530

    added Display.java program

---
+++ Display.java
@@ -0,0 +1,9 @@
+package addressBookSystem;
+
+public class Display {
+    static String welcomeMessage = "Welcome to Address Book Program";
+
+    public static void main(String[] args) {
+        System.out.println(welcomeMessage);
+    }
+}
